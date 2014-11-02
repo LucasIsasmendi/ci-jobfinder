3e543636c3a1e7f940cac0649d8af42f7416f151
LucasIsasmendi <tartalario@yahoo.com.ar>
2014-11-02 05:31:06 -0300

    First test

--- models/Job.js
+++ models/Job.js
@@ -7,13 +7,13 @@ var jobSchema = mongoose.Schema({
 
 var Job = mongoose.model('Job', jobSchema);
 
-exports.seedJobs = function() {
+exports.seedJobs = function(callback) {
 	Job.find({}).exec(function(error, collection){
 		if(collection.length === 0) {
 			Job.create({title:'Developer', description:'MEAN.js DevOps with AWS skills'});
 			Job.create({title:'Designer', description:'HTML5 CSS3 skills and video/tutorial'});
 			Job.create({title:'Customer Support', description:'Chinese customer support by phone and email'});
-			Job.create({title:'Financial', description:'Startup Account, GL, Analyst'});
+			Job.create({title:'Financial', description:'Startup Account, GL, Analyst'}, callback);
 		}
 	})
 }
